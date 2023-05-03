196f70ae6ba120c6d63ad9d4f1dbd2a13dea09de
Orchard <orchardyz@outlook.com>
2023-05-03 16:21:20 +0800

    fix: extension production packaging error

--- src/utils/loadUmiHTML.ts
+++ src/utils/loadUmiHTML.ts
@@ -14,7 +14,7 @@ const loadUmiHTML = (context: vscode.ExtensionContext, webviewPanel: vscode.Webv
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            ${isDev ? '' : '<link rel="stylesheet" href="${getUmiFilePath("umi.css")}" />'}
+            ${isDev ? '' : `<link rel="stylesheet" href="${getUmiFilePath('umi.css')}" />`}
         </head>
         <body>
             <div id="root"></div>
