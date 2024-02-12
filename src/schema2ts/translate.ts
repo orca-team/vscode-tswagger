479a5dbb4795fe1373eb54afd828104c38610b7a
Orchard <orchardyz@outlook.com>
2024-02-12 23:01:47 +0800

    feat: upgrade `bing-translate-api` to @4.x

--- src/schema2ts/translate.ts
+++ src/schema2ts/translate.ts
@@ -1,11 +1,11 @@
-import BingTranslate from 'bing-translate-api';
+import { translate as BingTranslate } from 'bing-translate-api';
 import { capitalize } from 'lodash-es';
 import { TranslateEngine } from '../types';
 import { setLocalTranslation } from '../utils/localTranslate';
 
 export const translate2EnByBing = async (text: string) => {
   try {
-    const result = await BingTranslate.translate(text, null, 'en');
+    const result = await BingTranslate(text, null, 'en');
     return result;
   } catch (err) {
     console.error('[BingTranslateAPI translated failed]: ', err);
@@ -23,7 +23,7 @@ export const formatTranslation = (translation: string) =>
 
 // TODO: 是否增加多种类型的翻译接口提供选择
 const translate = async (text: string, engine: TranslateEngine = 'Bing') => {
-  const translation = (await translate2EnByBing(text)).translation;
+  const translation = (await translate2EnByBing(text))?.translation ?? '';
   const formattedTranslation = formatTranslation(translation);
   setLocalTranslation(text, formattedTranslation);
   return formattedTranslation;
