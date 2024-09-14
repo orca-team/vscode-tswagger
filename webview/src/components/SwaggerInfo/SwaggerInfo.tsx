71317505c98ae51c0590706b4d4bb710674a72db
Orchard <orchardyz@outlook.com>
2024-09-14 21:01:18 +0800

    chore: adjust swagger info style

--- webview/src/components/SwaggerInfo/SwaggerInfo.tsx
+++ webview/src/components/SwaggerInfo/SwaggerInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { OpenAPIV2 } from 'openapi-types';
 import styles from './SwaggerInfo.less';
-import { Badge, Collapse, Descriptions, theme } from 'antd';
+import { Badge, Collapse, Descriptions, Tag, theme, Typography } from 'antd';
 import { CaretRightOutlined } from '@ant-design/icons';
 
 export interface SwaggerInfoProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -20,17 +20,31 @@ const SwaggerInfo: React.FC<SwaggerInfoProps> = (props) => {
           <Collapse.Panel key="swaggerOverview" header={`【文档信息】${v2Doc.info.title}`}>
             <Descriptions title="">
               <Descriptions.Item label="基本路径" span={3}>
-                {v2Doc.basePath}
+                <Typography.Text strong>{v2Doc.basePath}</Typography.Text>
               </Descriptions.Item>
               <Descriptions.Item label="描述信息" span={3}>
-                {v2Doc.info.description}
+                <div dangerouslySetInnerHTML={{ __html: v2Doc.info.description ?? '-' }}></div>
               </Descriptions.Item>
-              <Descriptions.Item label="API 版本">{v2Doc.info.version}</Descriptions.Item>
-              <Descriptions.Item label="服务器地址">{v2Doc.host}</Descriptions.Item>
+              <Descriptions.Item label="API 版本">
+                <Tag>{v2Doc.info.version}</Tag>
+              </Descriptions.Item>
+              <Descriptions.Item label="服务器地址">
+                <Typography.Text italic copyable>
+                  {v2Doc.host}
+                </Typography.Text>
+              </Descriptions.Item>
               <Descriptions.Item label="传输协议">{v2Doc.schemes?.join('、') ?? '-'}</Descriptions.Item>
               <Descriptions.Item label="联系者">{v2Doc.info.contact?.name ?? '-'}</Descriptions.Item>
               <Descriptions.Item label="联系地址">{v2Doc.info.contact?.url ?? '-'}</Descriptions.Item>
-              <Descriptions.Item label="联系邮箱">{v2Doc.info.contact?.email ?? '-'}</Descriptions.Item>
+              <Descriptions.Item label="联系邮箱">
+                {v2Doc.info.contact?.email ? (
+                  <Typography.Text underline copyable>
+                    {v2Doc.info.contact?.email}
+                  </Typography.Text>
+                ) : (
+                  '-'
+                )}
+              </Descriptions.Item>
             </Descriptions>
           </Collapse.Panel>
         </Collapse>
