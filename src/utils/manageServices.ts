6f559ba657e05978d36ae4cdfc30f8e0c0b1de56
Orchard <orchardyz@outlook.com>
2025-10-02 20:22:46 +0800

    style(eslint): allow console info level and clean up logs

--- src/utils/manageServices.ts
+++ src/utils/manageServices.ts
@@ -6,13 +6,10 @@ import * as vscode from 'vscode';
 export function manageServicesFromPanel(webview: vscode.Webview) {
   return function registerService(serviceMethod: string, callback: (params: any) => Promise<any>) {
     webview.onDidReceiveMessage(async (message) => {
-      console.info('[Message from umi webview]: ', message.method);
-      console.info(message);
       const { method, token, params } = message;
       if (serviceMethod === method) {
         try {
           const result = await callback(params);
-          console.log(`call ${method} success.`, token);
           webview.postMessage({
             token,
             method,
@@ -21,7 +18,6 @@ export function manageServicesFromPanel(webview: vscode.Webview) {
             success: true,
           });
         } catch (error: any) {
-          console.log(`call ${method} error.`);
           webview.postMessage({
             token,
             method,
