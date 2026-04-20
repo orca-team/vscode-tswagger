dca3809a349b096794cbfb3812cb051b642d409e
Orchard <orchardyz@outlook.com>
2026-04-20 15:07:28 +0800

    refactor(webview): simplify development workflow by removing dev mode conditionals and using build watch

--- src/utils/loadUmiHTML.ts
+++ src/utils/loadUmiHTML.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { getPathInVsCode, isDev } from './vscodeUtil';
+import { getPathInVsCode } from './vscodeUtil';
 
 const loadUmiHTML = (context: vscode.ExtensionContext, webviewPanel: vscode.WebviewPanel) => {
   const getUmiFilePath = (fileName: string) => {
@@ -14,7 +14,7 @@ const loadUmiHTML = (context: vscode.ExtensionContext, webviewPanel: vscode.Webv
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            ${isDev ? '' : `<link rel="stylesheet" href="${getUmiFilePath('umi.css')}" />`}
+          <link rel="stylesheet" href="${getUmiFilePath('umi.css')}" />
         </head>
         <body>
             <div id="root"></div>
